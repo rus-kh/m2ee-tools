rus-kh/m2ee-tools

commit efce29e97ec559db6db1748b42dcabc559d46da1

Replace optparse with argparse
It's 2018 now and optparse is deprecated.

src/m2ee.py
@@ -7,6 +7,7 @@
 #
 
 from __future__ import print_function
+import argparse
 import atexit
 import cmd
 import datetime
@@ -972,29 +973,30 @@ def start_console_logging(level):
 
 
 def main():
-    from optparse import OptionParser
-    parser = OptionParser()
-    parser.add_option(
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
         "-c",
+        nargs=1,
         action="append",
-        type="string",
         dest="yaml_files"
     )
-    parser.add_option(
+    parser.add_argument(
         "-v",
         "--verbose",
         action="count",
         dest="verbose",
+        default=0,
         help="increase verbosity of output (-vv to be even more verbose)"
     )
-    parser.add_option(
+    parser.add_argument(
         "-q",
         "--quiet",
         action="count",
         dest="quiet",
+        default=0,
         help="decrease verbosity of output (-qq to be even more quiet)"
     )
-    parser.add_option(
+    parser.add_argument(
         "-y",
         "--yolo",
         action="store_true",
@@ -1002,15 +1004,19 @@ def main():
         dest="yolo_mode",
         help="automatically answer all questions to run as non-interactively as possible"
     )
-    (options, args) = parser.parse_args()
+    parser.add_argument(
+        "onecmd",
+        nargs='*',
+    )
+    args = parser.parse_args()
 
     # how verbose should we be? see
     # http://docs.python.org/release/2.7/library/logging.html#logging-levels
-    verbosity = 0
-    if options.quiet:
-        verbosity = verbosity + options.quiet
-    if options.verbose:
-        verbosity = verbosity - options.verbose
+    verbosity = args.quiet - args.verbose
+    if args.quiet:
+        verbosity = verbosity + args.quiet
+    if args.verbose:
+        verbosity = verbosity - args.verbose
     verbosity = verbosity * 10 + 20
     if verbosity > 50:
         verbosity = 100
@@ -1019,12 +1025,12 @@ def main():
     start_console_logging(verbosity)
 
     cli = CLI(
-        yaml_files=options.yaml_files,
-        yolo_mode=options.yolo_mode,
+        yaml_files=args.yaml_files,
+        yolo_mode=args.yolo_mode,
     )
     atexit.register(cli._cleanup_logging)
-    if args:
-        cli.onecmd(' '.join(args))
+    if args.onecmd:
+        cli.onecmd(' '.join(args.onecmd))
     else:
         cli.cmdloop_handle_ctrl_c()
 

commit 5bcad8faabbc0f0bd5b236983fb688c0c324bde7

cli: Fix handling of logging verbosity
When switching to argparse and using a default of 0 for the quiet and
verbose options, the two if statements had to be removed, since they are
replaced by simply doing args.quiet - args.verbose now, because the
value is never None.

This resulted in e.g. -v not only showing DEBUG as extra, but also TRACE
already.

Fix this, and put the whole calculation on one line, while we're at it.

Fixes: efce29e97e ("Replace optparse with argparse")

src/m2ee.py
@@ -1002,12 +1002,7 @@ def main():
 
     # how verbose should we be? see
     # http://docs.python.org/release/2.7/library/logging.html#logging-levels
-    verbosity = args.quiet - args.verbose
-    if args.quiet:
-        verbosity = verbosity + args.quiet
-    if args.verbose:
-        verbosity = verbosity - args.verbose
-    verbosity = verbosity * 10 + 20
+    verbosity = (args.quiet - args.verbose) * 10 + 20
     if verbosity > 50:
         verbosity = 100
     if verbosity < 5: